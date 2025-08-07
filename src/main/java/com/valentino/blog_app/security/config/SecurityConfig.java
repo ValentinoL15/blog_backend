8f0c25bb76bc2634a88fceefd0d468529ffc5a3f
ValentinoL15 <pichilongo1@gmail.com>
2025-08-07 15:14:34 -0300

    roles

--- src/main/java/com/valentino/blog_app/security/config/SecurityConfig.java
+++ src/main/java/com/valentino/blog_app/security/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/login/**", "/api/auth/**", "/api/permissions/**", "/api/roles/**").permitAll()
+                        .requestMatchers( "/login/**", "/api/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 /*.oauth2Login(oauth2 -> oauth2
